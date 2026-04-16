a2433475dd09579c9af0b30cf727bb33a5a3615f
reynaldisw <reynaldisatriawanw@gmail.com>
2026-04-16 08:53:20 +0700

    feat: initialize root layout with custom fonts, metadata, and analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,8 @@ export default function RootLayout({
       className={`${plusJakartaSans.variable} ${beVietnamPro.variable} h-full antialiased`}
     >
       <head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="PlwrS0owkpYy9nbNzrW93w" async></script>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           rel="preload"
