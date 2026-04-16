03b06787b3529e8175aab710946b712cf74cd158
reynaldisw <reynaldisatriawanw@gmail.com>
2026-04-16 10:12:55 +0700

    feat: add automated deployment workflow and sitemap generation configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://juraganrentalsurabaya.com", // domain utama
+  generateRobotsTxt: true, // otomatis bikin robots.txt
+  sitemapSize: 5000, // optional, jumlah URL per sitemap file
+  changefreq: "daily", // optional
+  priority: 0.7, // optional
+};
